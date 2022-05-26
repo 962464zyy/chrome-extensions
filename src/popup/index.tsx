8f78cc4ba950d5272f05f4d2eb8c05a89b829e11
zhangyongyong <zhangyongyong@newrank.cn>
2022-05-26 13:28:04 +0800

    update:1

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,6 +1,6 @@
 // import React, { useEffect, useState } from "react";
 // import ReactDOM from "react-dom";
-import { Tabs, Typography } from "antd";
+import { Button, Input, Tabs, Typography } from "antd";
 import "../global.less";
 import "windi.css";
 import { useEffect, useState } from "react";
@@ -8,6 +8,18 @@ import ReactDOM = require("react-dom");
 import React = require("react");
 import { tabsOption } from "./sourceData";
 import styles from "./index.module.less";
+import Tool from "./pages/Tool";
+import Manage from "./pages/Manage";
+import SetPage from "./pages/SetPage";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+  Outlet,
+} from "react-router-dom";
+import NotLogin from "./pages/components/NotLogin";
+import NotFound from "./pages/components/NotFound";
 const { Link } = Typography;
 
 const { TabPane } = Tabs;
@@ -19,47 +31,39 @@ const Popup = () => {
   // 判断显示内容
   const getTabsDom = (key: string) => {
     const domObj = {
-      [tabsOption[0].key]: (
-        <div className={styles.g_flex}>
-          <Link href="https://www.newrank.cn/" title="新榜">
-            新榜
-          </Link>
-          <Link href="https://www.baidu.com/" title="百度">
-            百度
-          </Link>
-          <Link href="https://lpl.qq.com/" title="lol">
-            lol
-          </Link>
-          <Link href="https://zh.javascript.info/" title="js教程">
-            js教程
-          </Link>
-        </div>
-      ),
-      [tabsOption[1].key]: <div>管理</div>,
-      [tabsOption[2].key]: <div>设置</div>,
+      [tabsOption[0].key]: <Tool />,
+      [tabsOption[1].key]: <Manage />,
+      [tabsOption[2].key]: <SetPage />,
     };
     return domObj[key];
   };
 
   return (
     <div className="wd-w-362px wd-h-524px">
-      <div>
-        <Tabs centered>
-          {tabsOption.map((item: any) => (
-            <TabPane tab={item.label} key={item.key}>
-              {getTabsDom(item.key)}
-            </TabPane>
-          ))}
-        </Tabs>
+      <div className={styles.header}>
+        <NavLink to={"tool"}>工具</NavLink>
+        <NavLink to={"manage"}>管理</NavLink>
+        <NavLink to={"set"}>设置</NavLink>
       </div>
+      <Outlet />
     </div>
   );
 };
 
-console.log(document.getElementById("root"));
 ReactDOM.render(
   <React.StrictMode>
-    <Popup />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/popup.html" element={<Popup />}>
+          <Route index element={<Tool />}></Route>
+          <Route path="tool" element={<Tool />}></Route>
+          <Route path="manage" element={<Manage />}></Route>
+          <Route path="set" element={<SetPage />}></Route>
+          <Route path="notLogin" element={<NotLogin />}></Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
