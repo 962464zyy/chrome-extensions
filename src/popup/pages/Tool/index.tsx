4ac7259df2a8d43d1cf1c67f0de21fdd1e800fa0
zhangyongyong <zhangyongyong@newrank.cn>
2022-05-26 13:34:49 +0800

    update: 1

---
+++ src/popup/pages/Tool/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+type Props = {};
+
+const Tool: FC<any> = (props: Props) => {
+  return <div>Tool</div>;
+};
+
+export default Tool;
---
+++ src/popup/pages/components/NotFound/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+type Props = {};
+
+const NotFound: FC<any> = (props: Props) => {
+  return <div>NotFound</div>;
+};
+
+export default NotFound;
