0ae5fa716261fef6d917d1da4155d51710d77632
zhangyongyong <zhangyongyong@newrank.cn>
2022-05-26 13:45:59 +0800

    update: 1

--- src/popup/pages/Manage/index.tsx
+++ src/popup/pages/Manage/index.tsx
@@ -1,9 +1,23 @@
+import { Button, Input } from "antd";
 import { FC } from "react";
 
 type Props = {};
 
 const Manage: FC<any> = (props: Props) => {
-  return <div>Manage</div>;
+  const openOptions = () => {
+    console.log("openOptions");
+  };
+  const send = () => {
+    console.log("send");
+  };
+  return (
+    <div>
+      <Button onClick={openOptions}>打开options</Button>
+      <p>向content发送消息</p>
+      <Input placeholder="请输入内容" />
+      <Button onClick={send}>发送</Button>
+    </div>
+  );
 };
 
 export default Manage;
--- src/popup/pages/components/NotLogin/index.tsx
+++ src/popup/pages/components/NotLogin/index.tsx
@@ -1,9 +1,21 @@
+import { Button } from "antd";
 import { FC } from "react";
+import styles from "./index.module.less";
 
 type Props = {};
 
 const NotLogin: FC<any> = (props: Props) => {
-  return <div>NotLogin</div>;
+  const login = () => {
+    console.log("login");
+  };
+  return (
+    <div className={styles.container}>
+      <div>
+        <p>要使用小插件，请先登录哟</p>
+        <Button onClick={login}>登录</Button>
+      </div>
+    </div>
+  );
 };
 
 export default NotLogin;
