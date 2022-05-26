ff390d38f2d81704d5f8d94f84de401296db97e7
zhangyongyong <zhangyongyong@newrank.cn>
2022-05-26 13:53:01 +0800

    update: 1

--- src/popup/pages/Tool/index.tsx
+++ src/popup/pages/Tool/index.tsx
@@ -1,9 +1,18 @@
+import Link from "antd/lib/typography/Link";
 import { FC } from "react";
+import styles from "./index.module.less";
 
 type Props = {};
 
 const Tool: FC<any> = (props: Props) => {
-  return <div>Tool</div>;
+  return (
+    <div className={styles.container}>
+      <Link href="https://www.baidu.com/">百度</Link>
+      <Link href="https://www.baidu.com/">百度</Link>
+      <Link href="https://www.baidu.com/">百度</Link>
+      <Link href="https://www.baidu.com/">百度</Link>
+    </div>
+  );
 };
 
 export default Tool;
