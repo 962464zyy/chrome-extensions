649aa8b15503861e171ef2258a34bd55a31c1806
zhangyongyong <zhangyongyong@newrank.cn>
2022-07-14 09:12:13 +0800

    feat: 1

--- src/content_script.ts
+++ src/content_script.ts
@@ -8,6 +8,10 @@ const createPage = (textContent: string) => {
   container.style.width = "100px";
   container.textContent = textContent;
   container.style.backgroundColor = "red";
+  container.style.position = "fixed";
+  container.style.bottom = "0";
+  container.style.right = "0";
+  container.style.zIndex = "100";
   document.body.append(container);
 };
 // createPage();
@@ -15,11 +19,22 @@ const createPage = (textContent: string) => {
 document.addEventListener("DOMContentLoaded", function () {
   console.log("这是content script!");
 
-  if (location.href == "http://test.a.newrank.cn/center/#/user") {
-    console.log(`张勇勇的插件插的`);
-    createPage("textContent");
-  }
+  // if (location.href == "http://test.a.newrank.cn/center/#/user") {
+  //   console.log(`张勇勇的插件插的`);
+  //   createPage("textContent");
+  // }
+  // createPage("textContent");
 });
+if (location.href == "http://test.a.newrank.cn/center/#/user") {
+  console.log(`张勇勇的插件插的`);
+  const titleDomList = document.getElementsByTagName("title");
+  let titles = "";
+  Array.from(titleDomList).forEach((item: any) => {
+    titles += item.textContent + "\n";
+  });
+  createPage(titles);
+}
+// createPage("textContent");
 
 // 接收来自popup或者background的消息
 // @ts-ignore
@@ -28,14 +43,9 @@ chrome.runtime.onMessage.addListener(function (
   sender: any,
   sendResponse: any
 ) {
-  console.log(
-    "收到来自 " +
-      (sender.tab
-        ? "content-script(" + sender.tab.url + ")"
-        : "popup或者background") +
-      " 的消息：",
-    request
-  );
+  console.log("接收来自popup或者background的消息", request, sender);
   createPage(request.text);
-  sendResponse("我是content我收到你的消息了：" + JSON.stringify(request));
+  sendResponse(
+    "我是content_script,我收到你的消息了：" + JSON.stringify(request)
+  );
 });
